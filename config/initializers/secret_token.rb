6e3f2f8065cf98c6ba9d37a89d55fc795eb340f9
Christopher Wright <cmwright33@gmail.com>
2013-10-07 11:23:14 -0400

    frist commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Portfolio::Application.config.secret_token = '8c4e8c7039ea679b5b7f1b6f11f4f34aa6b4d6e923d6f50ce411da6b968ab2f6fccccbafa45f31997b5edf8276eedd3a6e8963f0a35d25914a3cb478b591a73e'
